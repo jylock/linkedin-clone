25b5cda4359433e4d0a83a1785638fbf2d25f70a
jylock <jsuo.mail@gmail.com>
2018-05-23 02:06:21 -0500

    current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+// Load User Profile
+const User = require('../../models/User');
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne( { user: req.user.id })
+      .then(profile => {
+        if(!profile) {
+          errors.nonprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;

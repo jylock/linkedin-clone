f0695c9d6187338d01016fb403ae01580ae515c3
jylock <jsuo.mail@gmail.com>
2018-05-23 15:51:50 -0500

    post create route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Post model
+const Post = require('../../models/Post');
+
+// validation
+const validatePostInput = require('../../validation/post');
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const {errors, isValid} = validatePostInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    // If any errors, send 400 with errors object
+    return res
+      .status(400)
+      .json(errors);
+  }
+
+  const newPost = new Post({text: req.body.text, name: req.body.name, avatar: req.body.avatar, user: req.user.id});
+
+  newPost
+    .save()
+    .then(post => res.json(post));
+});
 
 module.exports = router;

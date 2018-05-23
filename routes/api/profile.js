af4e27ed3848ee280fa57942afe901fb1d076cba
jylock <jsuo.mail@gmail.com>
2018-05-23 15:06:47 -0500

    delte education and experience routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -269,4 +269,72 @@ router.post('/education', passport.authenticate('jwt', {session: false}), (req, 
     })
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  Profile
+    .findOne({user: req.user.id})
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile
+        .experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+
+      // Splice out of array
+      profile
+        .experience
+        .splice(removeIndex, 1);
+
+      // save
+      profile
+        .save()
+        .then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  Profile
+    .findOne({user: req.user.id})
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile
+        .education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id);
+
+      // Splice out of array
+      profile
+        .education
+        .splice(removeIndex, 1);
+
+      // save
+      profile
+        .save()
+        .then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+  Profile
+    .findOneAndRemove({user: req.user.id})
+    .then(() => {
+      User
+        .findOneAndRemove({_id: req.user.id})
+        .then(() => res.json({success: true}));
+    });
+});
+
 module.exports = router;

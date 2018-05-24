1ddf58e8b6b4a08965febfd8c3f93f100625f68f
jylock <jsuo.mail@gmail.com>
2018-05-24 01:30:05 -0500

    react router setup

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
@@ -8,13 +9,13 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector
+                <h1 className="display-3 mb-4">LinkedIn Clone
                 </h1>
                 <p className="lead">
                   Create a developer profile/portfolio, share posts and get help from other developers</p>
                 <hr/>
-                <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
-                <a href="login.html" className="btn btn-lg btn-light">Login</a>
+                <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
               </div>
             </div>
           </div>

43776773114ac3f412bf9d9451596b85dd2f7da4
jylock <jsuo.mail@gmail.com>
2018-05-24 02:07:57 -0500

    testing registration with our form - no redux yet

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
   constructor() {
@@ -29,7 +30,9 @@ class Register extends Component {
       password2: this.state.password2
     }
 
-    console.log(newUser);
+    axios.post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   }
 
   render() {

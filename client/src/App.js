c5bc5226e6b8f200cc4c01d2eb6e1c281745438c
jylock <jsuo.mail@gmail.com>
2018-05-25 03:42:45 -0500

    logout and conditional navbar links

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -23,6 +23,17 @@ if(localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current profile
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -45,3 +45,13 @@ export const setCurrentUser = (decoded) => {
     payload: decoded
   }
 }
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which wil set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+}
